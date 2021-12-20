34c92a29df663398deb8e3ba7506b51c4f61b4bc
OleksandrTymoshenko <nomakievip@gmail.com>
2021-12-20 10:32:37 +0200

    fixed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,25 +8,19 @@ const refs = {
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
 function createGalleryMarkup(galleryItems) {
-    return galleryItems
-    .map(({ preview, original, description }) => {
+    return galleryItems.map(({ preview, original, description }) => {
         return `<a class="gallery__item" href="${original}">
             <img class="gallery__image"
                 src="${preview}"
-                alt="${description}"/>
+                alt="${description}"    />
         </a>`;
     })
     .join('');
 }
 refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-const onGalleryClick = event => {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')) {
-    return;
-}
-    new SimpleLightbox('.gallery a', {
+
+const asd = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
 });
-};
-refs.gallery.addEventListener('click', onGalleryClick);
+asd.on("show.simplelightbox",function (e){})
